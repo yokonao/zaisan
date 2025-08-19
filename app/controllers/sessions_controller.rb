2ac69de5aa3261102c8eb5ccb5a806b6e28db341
yokonao <yokotukanao@gmail.com>
2025-08-20 05:48:25 +0900

    ログインしましたメッセージは必要ないので削除

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to dashboard_path, notice: "ログインしました"
+      redirect_to dashboard_path
     else
       flash.now[:alert] = "メールアドレスまたはパスワードが正しくありません"
       render :new, status: :unprocessable_entity
